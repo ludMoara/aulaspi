d8a0f09846c8fa620e8e5e83350a6243813d2f68
LUDMYLLA MOARA DA SILVA ARRUDA CÂMARA <20231204010017@ifrn.local>
2026-09-01 15:45:29 -0300

    Configurando clase de modelo Evento

--- workspaces/pi/eventos/eventos/src/main/java/ifrn/pi/eventos/models/Evento.java
+++ workspaces/pi/eventos/eventos/src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -1,18 +1,37 @@
 package ifrn.pi.eventos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Evento {
 
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
 
+	
+	
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getLocal() {
