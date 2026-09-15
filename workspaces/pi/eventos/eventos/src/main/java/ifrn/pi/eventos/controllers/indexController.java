e2a01ae8eed45ff1c8d3f68a271006d791e23b4a
KAYKY SAMUEL VICTOR DA SILVA <20231204010053@ifrn.local>
2026-09-15 16:00:14 -0300

    Cadastrando e listando convidados

--- workspaces/pi/eventos/eventos/src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ workspaces/pi/eventos/eventos/src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -10,7 +10,6 @@ public class indexController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Chamou o método index");
-		return "home";
 		return "redirect:/eventos";
 	}
 
